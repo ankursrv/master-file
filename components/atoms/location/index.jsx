2190d0ad174514110846280aa481e4eeb86b1d91
ankur srivastava <ankur.srivastava@techchefz.com>
2026-01-23 13:37:29 +0530

    location

--- components/atoms/location/index.jsx
+++ components/atoms/location/index.jsx
@@ -1,16 +1,36 @@
 import React from "react"
+import Icons from "../icons"
+import { cn } from "@/lib/utils"
 
-const Location = ({ city, day }) => {
+const Location = ({
+  city,
+  day,
+  dotted = true,
+  devided,
+  icon1,
+  icon2,
+  className,
+}) => {
   return (
-    <div className="flex items-center gap-1 text-gray-600 text-sm mt-1">
-      <p>{city}</p>
-      { day &&
-        <span>
-          <svg viewBox="0 0 6 6" aria-hidden="true" className="dotted size-1">
-            <circle r="3" cx="3" cy="3"></circle>
-          </svg>
-        </span>}
-      <p>{day}</p>
+    <div className={cn("flex items-center gap-1 text-gray-600 text-sm mt-1", className)}>
+      <p className="flex items-center gap-2">
+        {icon1 && <Icons.Location />}
+        {city}
+      </p>
+      {dotted ?
+        <>
+          {day &&
+            <svg viewBox="0 0 6 6" aria-hidden="true" className="dotted size-1">
+              <circle r="3" cx="3" cy="3"></circle>
+            </svg>}
+        </>
+        :
+        devided && <span className="w-[1px] h-3 mx-1 bg-green-800" />
+      }
+      <p className="flex items-center gap-2">
+        {icon2 && <Icons.duration />}
+        {day}
+      </p>
     </div>
   )
 }
