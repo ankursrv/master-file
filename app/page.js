8baa598c33bc8c298a2c3a2b165ccfba877fd21c
Ankur Srivastava <ankur.srivastava@techchefz.com>
2026-06-04 15:21:47 +0530

    shadcn implement and use some component

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ export default function ShadcnPracticePage() {
               <DialogHeader>
                 <DialogTitle>Edit profile</DialogTitle>
                 <DialogDescription>
-                  Make changes to your profile here. Click save when you're done.
+                  Make changes to your profile here. Click save when you&apos;re done.
                 </DialogDescription>
               </DialogHeader>
               <div className="py-4">
